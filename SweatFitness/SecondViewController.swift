2189b3c50e2f9a0f4df6656fe0b0a5bd4ac5d7fb
Tai Won Chung <tchung1118@gmail.com>
2015-03-23 09:37:57 -0500

    Initial Commit

---
+++ SweatFitness/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SweatFitness
+//
+//  Created by Taiwon Chung on 3/23/15.
+//  Copyright (c) 2015 Sweat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
