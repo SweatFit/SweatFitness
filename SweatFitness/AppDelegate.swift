0f7fafd68d0c9fa5139d67cee3be74280c7776b5
Tai Won Chung <tchung1118@gmail.com>
2015-03-23 10:30:55 -0500

    code migration part5

--- SweatFitness/AppDelegate.swift
+++ SweatFitness/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+         Parse.setApplicationId("lTJ4n4RCNKDP505aMGNCD5dIyhdA3QwNC5HFiZgY", clientKey: "HPI9Je5p5NVZmBF9F0OMOVIPtlA2X2Tbz0SSit4c")
         return true
     }
 
--- SweatFitness/MainWorkoutViewController.swift
+++ SweatFitness/MainWorkoutViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class MainWorkoutViewController: UITableViewController {
     var workouts = WorkoutCollection()
