2c305e5273dd0c736ecbadbbd83acaf74d505628
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-03-29 21:47:29 -0500

    login background added

--- SweatFitness/LoginViewController.swift
+++ SweatFitness/LoginViewController.swift
@@ -13,12 +13,17 @@ class LoginViewController: PFLogInViewController, PFLogInViewControllerDelegate,
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.darkGrayColor() // TODO: Change Color later
+        self.view.backgroundColor = UIColor.purpleColor() // TODO: Change Color later
         
         let logoView = UIImageView(image: UIImage(named: "logo")) // TODO: Fix dimensions
         logoView.contentMode = UIViewContentMode.ScaleAspectFit
         self.logInView.logo = logoView
         self.delegate = self
+       /* self.fields = (PFLogInFieldsUsernameAndPassword
+            | PFLogInFieldsFacebook
+            | PFLogInFieldsTwitter);*/
+        
+        
         self.signUpController = SignUpViewController();
         
         signUpController.fields = (PFSignUpFields.UsernameAndPassword
@@ -43,30 +48,32 @@ class LoginViewController: PFLogInViewController, PFLogInViewControllerDelegate,
         var valid = true
         let email = info["email"] as String
         let password = info["password"] as String
-        let emailComps = split(email, ($0 = "@", allowEmptySlices: false)
-        var errMessage
+        let emailComps = split(email, {$0 == "@"}, allowEmptySlices: false)
+        var errMessage = ""
         
         
         // Password Sanity Check
-        if (password.utf16Count >= 8) {
+        if (password.utf16Count < 8) {
             errMessage = "Your password must be 8 characters or longer"
             valid = false
         }
         
-        
-        if !(email.hasSuffix("northwestern.edu") || email. {
+        // Email check
+        if !(email.hasSuffix("northwestern.edu") && emailComps.count == 2) {
+            println("User is trying to bullshit")
             errMessage = "You must use northwestern.edu email"
             valid = false
         }
         
-        else {
-            let alertView = UIAlertView(title: "Sign-up failed", message: "You must use u.northwestern.edu email", delegate: self, cancelButtonTitle: "Ok")
+        // Firstname, lastname check
+            
+        if (valid) {
+            return true
+        } else {
+            let alertView = UIAlertView(title: "Sign-up failed", message: errMessage, delegate: self, cancelButtonTitle: "Ok")
             alertView.show()
             return false
         }
-        // TODO: Check password for check
-        
-        //return false
     }
     
     
@@ -76,82 +83,6 @@ class LoginViewController: PFLogInViewController, PFLogInViewControllerDelegate,
         self.performSegueWithIdentifier("loginMainSegue", sender: self)
     }
     
-    //TODO 
-    /*
-        1. Check for u.northwestern.
-        2. FB Signup Enable
-        3. More Field for Signup (First name, Last name,
-    */
-    
-    /*
-    var usernameTF: UITextField?
-    var passwordTF: UITextField?
-    
-    override func viewDidDisappear(animated: Bool) {
-        var logInController:LoginViewController  = LoginViewController()
-        logInController.fields = (PFLogInFields.UsernameAndPassword
-            | PFLogInFields.Facebook
-            | PFLogInFields.Twitter
-            | PFLogInFields.LogInButton
-            | PFLogInFields.SignUpButton
-            | PFLogInFields.PasswordForgotten
-            | PFLogInFields.DismissButton
-        )
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        var logInController:LoginViewController  = LoginViewController()
-        logInController.fields = (PFLogInFields.UsernameAndPassword
-            | PFLogInFields.Facebook
-            | PFLogInFields.Twitter
-            | PFLogInFields.LogInButton
-            | PFLogInFields.SignUpButton
-            | PFLogInFields.PasswordForgotten
-            | PFLogInFields.DismissButton
-        )
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    @IBAction func login(sender: AnyObject) {
-        println(usernameTF!.text)
-        println(passwordTF!.text)
-        PFUser.logInWithUsernameInBackground(usernameTF!.text, password: passwordTF!.text) { (userObj: PFUser!, error: NSError!) -> Void in
-            if userObj == nil {
-                println(error)
-            } else {
-                println(userObj)
-            }
-        }
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            let unamecell = tableView.dequeueReusableCellWithIdentifier("usernameCell") as UITableViewCell
-            self.usernameTF = unamecell.contentView.viewWithTag(1) as UITextField?
-            self.usernameTF!.delegate = self
-            return unamecell
-        } else {
-            let pwdcell = tableView.dequeueReusableCellWithIdentifier("passwordCell") as UITableViewCell
-            self.passwordTF = pwdcell.contentView.viewWithTag(1) as UITextField?
-            self.passwordTF!.delegate = self
-            return pwdcell
-        }
-    }
-    
-    func textFieldDidEndEditing(textField: UITextField) {
-        textField.resignFirstResponder()
-    }
-    */
 }
 
 
--- SweatFitness/SignUpViewController.swift
+++ SweatFitness/SignUpViewController.swift
@@ -2,7 +2,7 @@
 //  SignUpViewController.swift
 //  SweatFitness
 //
-//  Created by shmoop on 3/28/15.
+//  Created by me on 3/28/15.
 //  Copyright (c) 2015 Sweat. All rights reserved.
 //
 
@@ -13,9 +13,19 @@ import Parse
 class SignUpViewController : PFSignUpViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set Placeholder
         self.signUpView.usernameField.placeholder = "Username (ex. fituser1)"
         self.signUpView.passwordField.placeholder = "Password (At least 8 characters)"
         self.signUpView.emailField.placeholder = "Email (ex. fitfit@u.northwestern.edu)"
         self.signUpView.additionalField.placeholder = "Full Name (ex: Fit User)"
+        
+        // Change the default logo to our logo
+        let logoView = UIImageView(image: UIImage(named: "logo"))
+        logoView.contentMode = UIViewContentMode.ScaleAspectFit
+        self.signUpView.logo = logoView
+        
+        // Set Background color
+        self.view.backgroundColor = UIColor.purpleColor()
     }
 }
