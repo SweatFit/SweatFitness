923414f01de0400b10225567087548a960160b02
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-03-30 01:31:59 -0500

    request base class file added

---
+++ SweatFitness/RequestDatasource.swift
@@ -0,0 +1,18 @@
+//
+//  RequestDatasource.swift
+//  SweatFitness
+//
+//  Created by shmoop on 3/30/15.
+//  Copyright (c) 2015 Sweat. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+/* BaseRequest
+    Base class for all requests
+
+*/
+class BaseRequest {
+    
+}
