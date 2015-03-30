9ec1f0dd4ca367691568e2f8e97ee3ed1af2d07b
Tai Won Chung <tchung1118@gmail.com>
2015-03-30 03:06:56 -0500

    started profile vc

---
+++ SweatFitness/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  SweatFitness
+//
+//  Created by Taiwon Chung on 3/30/15.
+//  Copyright (c) 2015 Sweat. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
